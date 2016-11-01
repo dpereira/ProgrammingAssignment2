5c80f90df1fe73d3ded7cc6fe34b40e8ecc67233
Diego Pereira <rockin.jack@gmail.com>
2016-11-01 00:01:25 -0200

    feat: add solution for matrix inversion w/ caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Solves and caches the solution for the inverse of a given matrix
 
-## Write a short comment describing this function
+## Creates cached matrix structure
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Solves the matrix, by trying to use a chached solution first, if available
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+       m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
